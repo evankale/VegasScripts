673ccadbfab04a6f5f59163ce1e9f9a265006644
Evan Kale <evankale91@gmail.com>
2017-04-24 00:31:13 -0400

    added SelectLeftRightOfCursor, setCursorLeftRightOfSelction

---
+++ Scripts/SelectAllLeftOfCursor.cs
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2017 Evan Kale
+ * Email: EvanKale91@gmail.com
+ * Web: www.youtube.com/EvanKale
+ * Social: @EvanKale91
+ *
+ * This file is part of VegasScripts.
+ *
+ * VegasScripts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//Selects all TrackEvents left of cursor.
+
+using System;
+using System.IO;
+using ScriptPortal.Vegas;
+using EKVegas;
+
+class EntryPoint
+{	
+	Vegas vegas = null;
+	Util util = null;
+
+	public void FromVegas(Vegas vegas)
+	{
+		this.vegas = vegas;
+		this.util = new Util(vegas);
+        
+		foreach (Track track in vegas.Project.Tracks)
+		{
+			if(track.IsValid())
+			{
+				foreach (TrackEvent trackEvent in track.Events)
+				{
+					if(util.IsTimecodeAfterTrackEvent(trackEvent, vegas.Cursor, true))
+					{
+						trackEvent.Selected = true;
+					}
+					else
+					{
+						trackEvent.Selected = false;
+					}
+				}
+
+			}
+		}
+	}
+}
---
+++ Scripts/SetCursorRightOfSelection.cs
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2017 Evan Kale
+ * Email: EvanKale91@gmail.com
+ * Web: www.youtube.com/EvanKale
+ * Social: @EvanKale91
+ *
+ * This file is part of VegasScripts.
+ *
+ * VegasScripts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//Sets the cursor to the right of the rightmost selected TrackEvent
+
+using System;
+using System.IO;
+using ScriptPortal.Vegas;
+
+class EntryPoint
+{
+    Vegas vegas = null;
+
+    public void FromVegas(Vegas vegas)
+    {
+        this.vegas = vegas;
+
+        Timecode rightMostTimecode = null;
+
+        foreach (Track track in vegas.Project.Tracks)
+        {
+            if (track.IsValid())
+            {
+                foreach (TrackEvent trackEvent in track.Events)
+                {
+                    if(trackEvent.Selected && (rightMostTimecode == null || trackEvent.End > rightMostTimecode))
+                    {
+                        rightMostTimecode = trackEvent.End;
+                    }
+                }
+            }
+        }
+
+        if(rightMostTimecode != null)
+        {
+            vegas.Cursor = rightMostTimecode;
+        }
+    }
+}
